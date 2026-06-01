c22198333a802ac4a077d4c49c90e9b08dc9f5a0
al-rezaie <ar.rezaie951@gmail.com>
2026-06-01 12:14:51 +0330

    MAIN LOOP AND FUNCTION STRUCTURE CREATED. (main.py)

---
+++ main.py
@@ -0,0 +1,40 @@
+from src import Categorized
+from src import Date
+from src import ID
+from src import IPv4
+from src import Number
+from src import Phone
+from src import Time
+
+def main():
+    print("\n1. Create a new random list\n2. exit\n")
+    action = int(input())
+    
+    if action == 2:
+        exit()
+        
+    print("\nWhat type of data you want to generate? Pleas select by typing the number:")
+    print("1.Categorized data\n2.Date\n3.Unique IDs\n4.IPv4\n5.Number\n6.Phone Number\n7.Random Time\n")
+    data_type = int(input())
+    
+    match data_type:
+        case 1:
+            print("Categorized")
+        case 2:
+            print("Date")
+        case 3:
+            print("ID")
+        case 4:
+            print("IPv4")
+        case 5:
+            print("Number")
+        case 6:
+            print("Phone")
+        case 7:
+            print("Time")
+            
+            
+if __name__ == "__main__":
+    print("Welcome to Random Data Generator. Please choose your action from the options below by typing the number:")
+    while True:
+        main()
